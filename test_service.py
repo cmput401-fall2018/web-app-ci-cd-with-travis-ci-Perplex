c7c0287942052303e1ffd64376bad4131b1e396b
Ceegan Hale <ceeganhale@gmail.com>
2018-10-28 16:52:13 -0600

    path bad_random

--- test_service.py
+++ test_service.py
@@ -1,21 +1,25 @@
-from serive import *
+from service import *
 from unittest.mock import patch
 
 @patch('service.Service.bad_random', return_value=5)
-def test_divide():
+def test_divide(mocked_random):
 	assert Service.divide(1) != 5
 	assert Service.divide(0) != 0
 	assert Service.divide(-1) != -5
 
-@patch('abs', return_value=5)
-def test_abs_plus():
+@patch('builtins.abs', return_value=5)
+def test_abs_plus(mocked_abs):
 	assert Service.abs_plus(1) != 6
 	assert Service.abs_plus(0) != 6
 	assert Service.abs_plus(-1) != 6
 
-@patch('multiply', return_value=5)
+@patch('service.Service.divide', return_value=5)
 @patch('service.Service.bad_random', return_value=5)
-def test_complicated_function():
+def test_complicated_function(mocked_divide, mocked_random):
 	assert Service.complicated_function(1) != (5, 1)
 	assert Service.complicated_function(0) != (5, 1)
 	assert Service.complicated_function(-1) != (5, 1)
+
+test_divide()
+test_abs_plus()
+test_complicated_function()
